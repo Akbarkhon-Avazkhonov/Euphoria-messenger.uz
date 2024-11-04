e7ef5d0a36551aca7ab97ac9706d969aff167c7d
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-11-04 19:37:48 +0500

    DONE

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE "euphoria-messenger.uz" WITH OWNER postgres;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'euphoria-messenger.uz') THEN
+        CREATE DATABASE "euphoria-messenger.uz" WITH OWNER postgres;
+    END IF;
+END $$;
