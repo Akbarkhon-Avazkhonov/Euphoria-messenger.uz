19bb16f3ae571392a71fbd7e9fea366ab57bfa5b
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2025-01-07 19:35:15 +0500

    start

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // add image 
-    output: "standalone",
     images: {
         domains: ['localhost'],
     },
-    future: {
-        ignoreBuildErrors: true, // Игнорировать ошибки при сборке
-      },
 };
 
 export default nextConfig;
